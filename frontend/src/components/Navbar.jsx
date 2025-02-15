0dd08563bb68db01b2811936095935eb7400f819
farhan <chfarhanilyas550@gmail.com>
2025-02-15 09:26:25 +0500

    Fixed Link issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,19 +8,15 @@ const Navbar = ({ colorMode, toggleColorMode }) => {
     <div className="p-5 max-w-screen mx-auto dark:bg-gray-900 bg-gray-100">
       <div className="mx-auto flex justify-between items-center h-16 flex-col gap-4 sm:flex-row transition-colors">
         {/* Logo */}
-        <h1 className="text-center text-2xl sm:text-3xl font-bold uppercase ">
-          <Link target="_blank" rel="noopener noreferrer" to={"https://github.com/Farhan2001M/Prodiverse"} className="flex items-center gap-2"> 
-            <p className="bg-gradient-to-r from-cyan-400 to-blue-600 bg-clip-text text-transparent">
+        <h1 className="text-center text-2xl sm:text-3xl font-bold uppercase flex items-center gap-2">
+          <Link to="/">
+            <p className="bg-gradient-to-r from-cyan-400 to-blue-600 bg-clip-text text-transparent cursor-pointer">
               Prodiverse
             </p>
-            <img
-              src="/icons/appicon.png"
-              alt="App Icon"
-              className="w-9 h-9 animate-spin"
-              style={{ animation: "spin 5s linear infinite" }} 
-            />
-            {/* <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 256 168"><path fill="#ff2638" d="m85.327 83.997l85.327 83.996H0z"/><path fill="#ffc600" d="m0 0l85.327 83.997L170.654 0z"/><path fill="#0079f2" d="m85.341 83.997l85.327 83.996l85.327-83.996L170.668 0z"/></svg>  */}
           </Link>
+          <a href="https://github.com/Farhan2001M/Prodiverse"target="_blank" rel="noopener noreferrer" >
+            <img src="/icons/appicon.png" alt="App Icon" className="w-9 h-9 animate-spin cursor-pointer" style={{ animation: "spin 5s linear infinite" }} />
+          </a>
         </h1>
 
         {/* Buttons */}
@@ -30,15 +26,8 @@ const Navbar = ({ colorMode, toggleColorMode }) => {
               <FcPlus size={30}  />
             </button>
           </Link>
-          <button
-            onClick={toggleColorMode}
-            className="p-2 rounded-lg border  border-black dark:border-white hover:bg-gray-200 dark:hover:bg-gray-700"
-          >
-            {colorMode === "light" ? (
-              <IoMoon size={30} className="text-black" />
-            ) : (
-              <LuSun size={30}  className="text-white" />
-            )}
+          <button onClick={toggleColorMode} className="p-2 rounded-lg border  border-black dark:border-white hover:bg-gray-200 dark:hover:bg-gray-700" >
+            {colorMode === "light" ? ( <IoMoon size={30} className="text-black" /> ) : ( <LuSun size={30}  className="text-white" /> )}
           </button>
         </div>
       </div>
